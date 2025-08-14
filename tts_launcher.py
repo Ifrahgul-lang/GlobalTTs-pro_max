d123e355ba184b8438b4126ee29c8df4a43c3179
Ifrahgul-lang <ifrahgulf23@nutech.edu.pk>
2025-08-14 12:01:01 -0700

    Add files via upload

---
+++ tts_launcher.py
@@ -0,0 +1,7 @@
+﻿from pyngrok import ngrok
+import os
+
+port = 8501
+public_url = ngrok.connect(port)
+print("🌍 Public link to share:", public_url)
+os.system(f"streamlit run tts_app.py --server.port {port}")
